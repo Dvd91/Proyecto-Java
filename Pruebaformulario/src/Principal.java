6895fda952bb0e3bd4dc7379b9538719cfa17904
ecarpintero <eduardocarpinterorodriguez@gmail.com>
2014-03-28 18:06:30 +0100

    prueba formulario

---
+++ Pruebaformulario/src/Principal.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alumno
+ */
+public class Principal {
+ public static void main(String[] args) {
+     Formulario f=new Formulario();
+     f.setVisible(true);
+ }
+}
